4e5d9e36f163e44f9032d6bc3d42919ff29f1a7a
Han Han <haannn@qq.com>
2024-12-16 15:00:55 +0000

    feat: new algo

--- libs/he24_algorithm/src/index.test.ts
+++ libs/he24_algorithm/src/index.test.ts
@@ -1,16 +1,23 @@
 import {describe, expect, test} from "vitest";
-import {Solution} from "./index.ts";
+import {Solution, SolutionNode, suan_list} from "./index.ts";
 
 describe('ability', () => {
     function test_he24(a: number,b: number,c: number,d: number) {
         const solution = new Solution()
         solution.real_solution(24,[a,b,c,d])
-        expect(solution.result_list.length > 0)
+        expect(solution.result_list.length > 0).equal(true)
         for (const item of solution.result_list) {
             expect(solution.equal(eval(solution.map_str(item)),24)).equal(true)
         }
     }
-    test('work', () => {
+    test('simple work', () => {
+        test_he24(4,4,4,4)
+    })
+    test('simple work2', () => {
+        test_he24(1,1,1,12)
+        test_he24(1,1,1,13)
+    })
+    test.skip('work', () => {
         test_he24(4,4,4,4)
         test_he24(2,2,5,5)
         test_he24(1,2,3,4)
@@ -21,5 +28,23 @@ describe('ability', () => {
         test_he24(1,6,11,13)
         test_he24(2,5,5,10)
         test_he24(3,3,8,8)
+    })
+})
+
+describe('SolutionNode', () => {
+    test('empty SolutionNode', () => {
+        expect(new SolutionNode(0).getResult()).equal(0)
+    })
+    test('initia SolutionNode', () => {
+        const solutionNode = new SolutionNode()
+        solutionNode.result = 3
+        expect(solutionNode.getResult()).equal(3)
+    })
+    test('SolutionNode op', () => {
+        const solutionNode = new SolutionNode()
+        solutionNode.left = new SolutionNode(3)
+        solutionNode.right = new SolutionNode(4)
+        solutionNode.op = suan_list.find(it => it.label = 'add')
+        expect(solutionNode.getResult()).equal(7)
     })
 })
--- libs/he24_algorithm/src/index.ts
+++ libs/he24_algorithm/src/index.ts
@@ -1,5 +1,5 @@
 const start_suan = {label: 'start', suan: (_left: number, right: number) => {return right}, map_str: (_left: any, right: any) => `${right}`}
-const suan_list = [
+export const suan_list = [
 
     {label: 'add', suan: (left: number, right: number) => {return left + right}, map_str: (left: any, right: any) => `(${left} + ${right})`},
     {label: 'minus', suan: (left: number, right: number) => {return left - right}, map_str: (left: any, right: any) => `(${left} - ${right})`},
@@ -15,6 +15,7 @@ export class Solution {
     nums = [1,2,3,4]
     used: {index: number, label: string}[] = []
     result_list: {index: number, label: string}[][] = []
+    result_list2: SolutionNode[] = []
     equal(a: number, b: number) {
         if (Math.abs(a - b) < 0.00001) {
             return true
@@ -27,10 +28,42 @@ export class Solution {
         this.result_list = []
         this.target = target
         this.nums = nums
-        this.solution()
+        // this.solution()
+        this.solution2(nums.map(it => new SolutionNode(it)))
         this.print_solution_list()
     }
 
+
+    solution2(nums: SolutionNode[]) {
+        
+        for (let i = 0; i < nums.length; ++i) {
+            const pre = [...nums]
+            const first = nums.splice(i, 1)[0]
+            
+            for (let k = 0; k < nums.length; ++k) {
+                const pre2 = [...nums]
+                const second = nums.splice(k, 1)[0]
+                for (let j = 0; j < suan_list.length; ++j) {
+                    let suan1 = suan_list[j]
+                    const {label, suan} = suan1
+                    const result = new SolutionNode()
+                    result.left = first
+                    result.right = second
+                    result.op = suan1
+                    nums.push(result)
+                    if (nums.length !== 1) {
+                        this.solution2(nums)
+                    }
+                    if (this.equal(this.target, result.getResult())) {
+                        this.result_list2.push(result)
+                    }
+                }
+                nums = pre2
+            }
+            nums = pre
+            
+        }
+    }
     solution(current: null | number = null) {
         for (let i = 0; i < this.nums.length; ++i) {
             if (this.used.find(it => it.index === i)) {
@@ -95,3 +128,21 @@ export class Solution {
 // a.real_solution(24,[1,6,11,13])
 // a.real_solution(24,[2,5,5,10])
 // // a.real_solution(24, [3,3,8,8])
+
+export class SolutionNode {
+    left?: SolutionNode
+    right?: SolutionNode
+    op?: (typeof suan_list)[number]
+    result = 0
+    constructor(result?: number) {
+        this.result = result
+    }
+    getResult(): number {
+        if (!this.left || !this.right || !this.op) {
+            return this.result
+        }
+        return this.op.suan(this.left.getResult(), this.right.getResult())
+
+    }
+
+}

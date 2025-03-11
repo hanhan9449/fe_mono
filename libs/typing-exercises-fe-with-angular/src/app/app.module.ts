5d614633cf436e184a4a039ebeae74597c683d9b
hanhan <haannn@qq.com>
2025-03-12 01:49:30 +0800

    feat: update to standalone 5

--- libs/typing-exercises-fe-with-angular/src/app/app.module.ts
+++ libs/typing-exercises-fe-with-angular/src/app/app.module.ts
@@ -2,9 +2,9 @@ import {Injector, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { KeyboardModule } from './keyboard/keyboard.module';
-import { InputExerciseModule } from './input-exercise/input-exercise.module';
-import { SpeedCountModule } from './speed-count/speed-count.module';
+
+
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -13,19 +13,16 @@ import {createCustomElement} from "@angular/elements";
 @NgModule({
   declarations: [],
   imports: [
-      AppComponent,
+    AppComponent,
     BrowserModule,
-    KeyboardModule,
-    InputExerciseModule,
-    SpeedCountModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
     }),
-  ],
+],
   providers: [],
   bootstrap: [AppComponent],
 })

38d6cccd02c517ba2470da4b5308b41f0c9f2403
hanhan <haannn@qq.com>
2025-03-13 00:55:12 +0800

    feat: support cloudflare2

--- apps/awemeblog/astro.config.mjs
+++ apps/awemeblog/astro.config.mjs
@@ -27,5 +27,14 @@ export default defineConfig({
   solidJs(), sitemap()],
   adapter: cloudflare(),
   site: 'https://next.hanhan9449.top',
-  output: 'server'
+  output: 'server',
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });

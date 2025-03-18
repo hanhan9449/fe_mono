0f318006fdc975ec3075636fa1f838beea4a571f
hanhan <haannn@qq.com>
2025-03-19 00:23:37 +0800

    feat: sentry support

--- apps/awemeblog/astro.config.mjs
+++ apps/awemeblog/astro.config.mjs
@@ -17,12 +17,24 @@ import netlify from "@astrojs/netlify";
 
 import sitemap from "@astrojs/sitemap";
 
+import sentry from "@sentry/astro";
+
+const sentryIntegration = sentry({
+      dsn: "https://8c46c1788ebe5f44f8a7d27c67952beb@o4508999636942848.ingest.us.sentry.io/4508999640547328",
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    })
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), // alpinejs(),
-  // preact(),
-  react(), svelte(),
-  solidJs(), sitemap()],
+  integrations: [// alpinejs(),
+  mdx(), // preact(),
+  react(), svelte(), solidJs(), sitemap(), sentryIntegration],
   adapter: netlify(),
   site: 'https://next.hanhan9449.top'
 });

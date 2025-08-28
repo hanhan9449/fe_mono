a357db5be17321d218ae5dbc2af7378342d6656a
hanhan <haannn@qq.com>
2025-08-28 23:09:43 +0800

    feat: test

--- apps/secret-store/src/App.tsx
+++ apps/secret-store/src/App.tsx
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const App = () => {
   const [id, setId] = useState('');
   const [appName, setAppName] = useState('');
   const [password, setPassword] = useState('');
+  const [hiddenValue, setHiddenValue] = useState('');
+  useEffect(() => {
+    const unionId = `(${appName}) ${id}`
+    setHiddenValue(unionId)
+
+  }, [id, appName]);
+  const submitButtonRef = useRef<HTMLButtonElement>(null)
+  function handleFormSubmit(e) {
+    e.preventDefault()
+  }
   function handleSave() {
     if (!appName || !id || !password) {
       return
@@ -21,12 +31,14 @@ const App = () => {
     navigator.credentials.store(credential).then(() => console.log('Save Success'))
   }
   return (
-    <div style={{display: 'flex', flexDirection: 'column', width: 'fit-content'}}>
+    <form onSubmit={handleFormSubmit} style={{display: 'flex', flexDirection: 'column', width: 'fit-content'}}>
+      <input value={hiddenValue} type={'text'} name={'username'} style={{position: 'absolute', clipPath: 'circle(1px)'}}/>
       <label htmlFor="idInput"> Id <input id="idInput" value={id} onChange={(e) => setId(e.target.value)} /> </label>
       <label htmlFor="appNameInput"> appName <input id="appNameInput" value={appName} onChange={(e) => setAppName(e.target.value)} /> </label>
-      <label htmlFor="passwordInput"> password <input id="passwordInput" value={password} onChange={(e) => setPassword(e.target.value)} /> </label>
+      <label htmlFor="passwordInput"> password <input id="passwordInput" value={password} type={'password'} name={password} onChange={(e) => setPassword(e.target.value)} /> </label>
       <button onClick={handleSave}>Submit to save</button>
-    </div>
+      <button ref={submitButtonRef} type={'submit'}>Submit by form</button>
+    </form>
   );
 };
 
